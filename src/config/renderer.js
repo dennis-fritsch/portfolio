363f8fbb775719f32363683d36d1f5a357dda770
Fridge-o <dev.dennis.fritsch@gmail.com>
2022-08-06 23:53:57 +0200

    Added table to renderer

--- src/config/renderer.js
+++ src/config/renderer.js
@@ -1,7 +1,16 @@
-import { Typography } from 'antd'
+import { Typography, Row } from 'antd'
+import styled from 'styled-components'
 import { COLORS } from 'config/styles'
 
 const { Title, Text } = Typography
+
+const StyledTable = styled('table')`
+  border: 1px solid ${COLORS.secondary};
+`
+
+const StyledCell = styled('td')`
+  padding: 0.5rem;
+`
 
 // https://www.npmjs.com/package/@graphcms/rich-text-react-renderer
 export const richTextRenderer = {
@@ -12,6 +21,12 @@ export const richTextRenderer = {
   h5: ({ children }) => <Title level={5}>{children}</Title>,
   bold: ({ children }) => <strong>{children}</strong>,
   p: ({ children }) => <Text>{children}</Text>,
+  table: ({ children }) => <StyledTable>{children}</StyledTable>,
+  table_cell: ({ children }) => {
+    console.log('children', children)
+    return <StyledCell>{children}</StyledCell>
+  },
+
   code: ({ children }) => (
     <Text
       style={{
